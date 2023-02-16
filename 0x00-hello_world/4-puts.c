2e9e261f23c9de4fa056fee2f889c03facdc963a
eleni <eleniababu@gmail.com>
2023-02-16 10:51:27 -0800

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
+/*prints programming is like building a multilingual puzzle */
+
 #include <stdio.h>
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
