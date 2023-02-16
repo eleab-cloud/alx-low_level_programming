f7072ee12dc6b43c7d1dd0c3fed1ecdac49cb62a
eleni <eleniababu@gmail.com>
2023-02-16 10:56:09 -0800

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,19 @@
-/*prints programming is like building a multilingual puzzle */
+/*
+ * File: 4-puts.c
+ * Auth: eleab
+ */
 
 #include <stdio.h>
-int main (void)
-{
-	puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
