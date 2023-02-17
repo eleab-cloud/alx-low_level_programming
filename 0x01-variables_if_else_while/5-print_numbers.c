50f0cdaad6bb55e18da38601804d88edf37eac85
eleni <eleniababu@gmail.com>
2023-02-17 10:28:51 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'using putchar'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10; num++)
+	{
+	printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
