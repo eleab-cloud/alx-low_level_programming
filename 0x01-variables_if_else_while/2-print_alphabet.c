14d96d77b28d6e96aa6f15bb26593e65e3c105e4
eleni <eleniababu@gmail.com>
2023-02-17 09:23:07 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'using putchar'
+* Return: Always 0 (Success)
+*/
+char main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	return (0);
+}
