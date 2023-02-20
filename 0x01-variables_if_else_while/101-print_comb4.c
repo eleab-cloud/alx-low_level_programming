8a6a682c433bec8b48432d01d61115deb6114d47
eleni <eleniababu@gmail.com>
2023-02-19 23:46:19 -0800

    3 digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'using putchar with four times'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 8; i++)
+	{
+		for (j = 1; j < 9; j++)
+		{
+			for (k = 2; k < 10; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar (48 + i);
+					putchar (48 + j);
+					putchar (48 + k);
+					if (i != 8)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
