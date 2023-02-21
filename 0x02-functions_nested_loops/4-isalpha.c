be91f53c0a935ce490ff28031ca30ab858af6e60
eleni <eleniababu@gmail.com>
2023-02-21 08:10:39 -0800

    alppha or num

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _isalpha - check if a char is alphabet or not
+ * @c: is char to be checked
+ * Return: 1 if char is a lettere, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
