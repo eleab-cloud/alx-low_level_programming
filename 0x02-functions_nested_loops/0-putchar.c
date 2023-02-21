1f12b1553c6ad88d1b3174fe0b22530a992cfba8
eleni <eleniababu@gmail.com>
2023-02-20 22:55:44 -0800

    putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'prints _putchar'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+	return (0);
+}
