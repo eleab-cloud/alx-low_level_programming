3072a3a2cc5c21c044a33b0e74d0eb30170677de
eleni <eleniababu@gmail.com>
2023-02-21 04:06:10 -0800

    still on puchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,20 +5,13 @@
 * Description: 'prints alphabet a to z'
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar (i);
+		_putchar (i);
 	}
-	putchar ('\n');
+	_putchar ('\n');
 }
