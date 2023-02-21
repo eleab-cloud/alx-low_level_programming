b37fe6ffbf7dc8215c8591532cc3a84a61873c7d
eleni <eleniababu@gmail.com>
2023-02-21 08:33:55 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *print_sign - check if a char is alphabet or not
+ * @n: is char to be checked
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(n);
+		return (0);
+	}
+}
