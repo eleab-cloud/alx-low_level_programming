e6b0e4b7f5835ac92fd05831e98c4d787cb9865d
eleni <eleniababu@gmail.com>
2023-02-21 21:20:46 -0800

    time tale

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - print time table
+ *
+ * Return:no return
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(i * j + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
