d39ef571f7b17d1b9d8c17cc62ebeda999b8c764
eleni <eleniababu@gmail.com>
2023-02-22 01:19:53 -0800

    n times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - print time tabile
+ * @n: number to be considered
+ *
+ * Return:no return
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int x;
+	int q = n + 1;
+
+	if (n < 16 && n >= 0)
+	{
+		for (i = 0; i < q; i++)
+		{
+			for (j = 0; j < q; j++)
+			{
+				x = i * j;
+
+				if (j == 0)
+					{
+						_putchar(x + '0');
+					}
+				if (x < 10 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(x + '0');
+					}
+				else if (x >= 10 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(x / 10 + '0');
+						_putchar(x % 10 + '0');
+					}
+			}
+		_putchar('\n');
+		}
+	}
+	else if (n > 15 && n < 0)
+	{
+		return ();
+	}
+}
