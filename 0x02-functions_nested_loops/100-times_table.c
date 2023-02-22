ee50f5ff13b04896b77621f3bcca7eb911fde67c
eleni <eleniababu@gmail.com>
2023-02-22 04:20:41 -0800

    n times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,14 +3,11 @@
 /**
  * print_times_table - print time tabile
  * @n: number to be considered
- *
  * Return:no return
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int x;
+	int i, j, x;
 	int q = n + 1;
 
 	if (n < 16 && n >= 0)
@@ -20,37 +17,33 @@ void print_times_table(int n)
 			for (j = 0; j < q; j++)
 			{
 				x = i * j;
-
 				if (j == 0)
-					{
-						_putchar(x + '0');
-					}
-				else if (x < 10 && j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(x + '0');
-					}
-				else if (x >= 10 && x < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(x / 10 + '0');
-						_putchar(x % 10 + '0');
-					}
-				else if (x >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((x / 100) + '0');
-						_putchar(((x / 10) % 10) + '0');
-						_putchar((x % 10) + '0');
-					}
+				{
+					_putchar(x + '0');
+				} else if (x < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(x + '0');
+				} else if (x >= 10 && x < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(x / 10 + '0');
+					_putchar(x % 10 + '0');
+				} else if (x >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10) % 10) + '0');
+					_putchar((x % 10) + '0');
+				}
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
