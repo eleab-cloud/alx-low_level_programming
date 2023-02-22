e3663b0e8dafef4d7024cff7aec1dbc19bbe0f21
eleni <eleniababu@gmail.com>
2023-02-22 03:59:19 -0800

    n table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,9 +15,9 @@ void print_times_table(int n)
 
 	if (n < 16 && n >= 0)
 	{
-		for (i = 0; i < q; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < q; j++)
+			for (j = 0; j < n; j++)
 			{
 				x = i * j;
 
