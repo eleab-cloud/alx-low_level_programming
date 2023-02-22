4c9fae623efdc6cbceeb955cbd34c946fce010eb
eleni <eleniababu@gmail.com>
2023-02-21 21:35:11 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,15 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int x;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(i * j + '0');
+			x = i * j;
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
