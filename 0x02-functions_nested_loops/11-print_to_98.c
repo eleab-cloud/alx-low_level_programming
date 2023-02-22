5045cc96d73f2a36d8419558ff07c225c776aa41
eleni <eleniababu@gmail.com>
2023-02-22 00:22:27 -0800

    print numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - add two numbers
+ * @n: to be printed
+ * Return: none
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+}
