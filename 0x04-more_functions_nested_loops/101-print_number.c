a117099f2cfc8835fdeed9ea464012f98f08a161
eleni <eleniababu@gmail.com>
2023-02-24 04:38:56 -0800

    intiger

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,16 +1,18 @@
-#include "main.h
+#include "main.h"
 #include <stdio.h>
 
 /**
-* print_number - prints integer
-* @n: input par
-*/
+ * print_number- prints integer
+ * @n: input para
+ */
 void print_number(int n)
 {
 	unsigned int n1;
+
 	if (n < 0)
 	{
 		n1 = -n;
+
 		_putchar('-');
 	}
 	else
