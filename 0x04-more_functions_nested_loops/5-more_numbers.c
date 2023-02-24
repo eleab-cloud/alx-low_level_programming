00b3f6399282950ab088e6209f38bd450857923d
eleni <eleniababu@gmail.com>
2023-02-23 23:34:43 -0800

    3 put

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			{
-				_putchar(j + '0');
-			}
-			else
-			{
-				_putchar(j / 10 + '0');
+			if (j >= 10)
+				_putchar('1');
 				_putchar(j % 10 + '0');
-			}
 		}
 		 _putchar('\n');
 	}
