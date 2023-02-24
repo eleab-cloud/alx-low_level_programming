37121757e69c74bfda4d4b6a229d017173c11d74
eleni <eleniababu@gmail.com>
2023-02-24 00:27:27 -0800

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,18 @@
  */
 void print_line(int n)
 {
+	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		if (n > 0)
-			_putchar('_');
-		putchar('\n');
+		_putchar('\n');
 	}
-	 _putchar('\n');
+	else if (n > 0)
+	{
+		for (i = i; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
