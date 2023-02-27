9fd6dd6f2cde7a9888552e76a4070d97f9457300
eleni <eleniababu@gmail.com>
2023-02-27 08:01:05 -0800

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts- prints a string
+ * @str: the string variable
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
