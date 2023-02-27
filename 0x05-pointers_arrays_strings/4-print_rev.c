4de862b6d5104043ad9bcaef2513bae435f95042
eleni <eleniababu@gmail.com>
2023-02-27 09:56:17 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev- prints a string
+ * @s: the string variable
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int r;
+
+	while (*s != '\0')
+	{
+		l++
+		s++;
+	}
+
+	s--;
+
+	for (r = l; r > 0; r--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
