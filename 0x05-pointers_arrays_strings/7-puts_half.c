7ac89d89a2581929d5f7bff7fc9abd166c2fda16
eleni <eleniababu@gmail.com>
2023-02-27 23:51:33 -0800

    half print

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half- prints every other char
+ * @str: the string variable
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int r = 0;
+	int i;
+
+	while (str[r] != '\0')
+	{
+		r++;
+	}
+	if (r % 2 == 0)
+	{
+		for (i = ((i / 2) + 1) ; i < r; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		r--;
+		for (i = r; i >= ((r - 1) / 2); i--)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
