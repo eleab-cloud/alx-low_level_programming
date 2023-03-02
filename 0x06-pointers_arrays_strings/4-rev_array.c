61ef449de748b17d4101eb08f87ce5f1fdee95ee
eleni <eleniababu@gmail.com>
2023-03-02 08:35:16 -0800

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,9 +13,9 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		rev[i} = a[i};
-		a[i] = a[n - 1];
-		a[n - 1] = rev[i];
 		n--;
+		rev[i} = a[i};
+		a[i] = a[n];
+		a[n] = rev[i];
 	}
 }
