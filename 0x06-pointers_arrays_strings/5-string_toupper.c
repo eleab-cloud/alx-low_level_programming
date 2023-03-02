f0d4be36d02db7174b6460ab8ff81560d9986a81
eleni <eleniababu@gmail.com>
2023-03-02 09:44:09 -0800

    small to cap

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  *string_toupper- change every lower to upper case
+ *  @n: the tring
+ *  Return: pointer
+ */
+char *string_toupper(char *n)
+{
+	int rev;
+
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i} <= 'z' && n[i] >= 'a')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++
+	}
+	return (n);
+}
