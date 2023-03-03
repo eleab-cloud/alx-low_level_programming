9b0072bff08523dd6bb69b2dd5ca7cd30eaa70d6
eleni <eleniababu@gmail.com>
2023-03-03 04:05:52 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  *rot13- apply rot 13 on the string
+ *  @s: the tring
+ *  Return: pointer
+ */
+char *rot13(char *s);
+{
+	int i, j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char nu[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = nu[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
