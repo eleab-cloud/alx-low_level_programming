6012d29bcc1ef6d1d2caab368fdb15b0d73247be
eleni <eleniababu@gmail.com>
2023-03-06 03:33:48 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset- fills n bytes of s  with b
+ * @n: the n bytes of s
+ * @s: the array
+ * @b: the constant char
+ * Return: a pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
