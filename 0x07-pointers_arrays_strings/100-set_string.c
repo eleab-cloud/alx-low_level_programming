3a3d678a8939ec958cf33fdd56e96bc18df1f87c
eleni <eleniababu@gmail.com>
2023-03-07 04:03:06 -0800

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string- fills n bytes of s  with b
+ * @s: pinter of a pointer
+ * @to: char pointer
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
