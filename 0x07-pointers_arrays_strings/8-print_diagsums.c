f3020a132ceee431a798a9bdab4da26c47c9ea58
eleni <eleniababu@gmail.com>
2023-03-07 03:48:24 -0800

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,19 +9,19 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i;
+	int i, j;
 	int sum = 0;
 	int sum1 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum = sum + a[i][i];
+		sum = sum + a[i * size + i];
 	}
-	for (i = 0; i < size; i++)
+	for (i = size - 1; i >= 0; i--)
 	{
-		sum1 = sum1 + a[i][size - 1];
-		size--;
+		j = size - 1;
+		sum1 = sum1 + a[i * size + (j - i)];
 	}
 	printf("%d, ", sum);
-	printf("%d", sum1);
+	printf("%d\n", sum1);
 }
