1a26f213e4fee631612c92c5732a918e78d93d92
eleni <eleniababu@gmail.com>
2023-03-08 11:20:10 -0800

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
+int realp(int nu, int i);
+
 /**
  * is_prime_number- tells whether prime nmber or not
  * @n: is the number
  * Return: 1 if prime and 0 if not
  */
-int realp(int nu, int i);
 int is_prime_number(int n)
 {
 	if (n <= 1)
@@ -16,11 +17,12 @@ int is_prime_number(int n)
 		return (realp(n, (n - 1)));
 	}
 }
+
 /**
- * realp-the real prime
+ * realp- the real prime
  * @nu: the number whose prime is needed
  * @i: counter
- * Return: 1 if prime 0 otherwise
+ * Return: 1 or 0
  */
 int realp(int nu, int i)
 {
