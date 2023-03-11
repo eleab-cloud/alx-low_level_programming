01263fdfe9a94fcd2e07b24355299534b4dc3dd4
eleni <eleniababu@gmail.com>
2023-03-11 11:10:23 -0800

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- intrance
+ * @argc: one arg
+ * @argv: another arg
+ * Return: always zero
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
