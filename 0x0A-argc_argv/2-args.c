cd5adbf41d13e8261ee2de3bf4941a33a2282539
eleab cloud <eleniababu@gmail.com>
2023-03-13 04:28:17 -0700

    all argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- intrance
+ * @argc: one arg
+ * @argv: another arg
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
