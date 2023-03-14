44e4a6d8463b4594a97ec144c5db00081edf1862
eleab cloud <eleniababu@gmail.com>
2023-03-14 04:05:40 -0700

    maloc task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * create_array- creates an array
+ * @size: size of arry
+ * @c: the char
+ * Return: a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str;
+	str = maloc(sizeof(char) * size);
+	
+	if (str == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
