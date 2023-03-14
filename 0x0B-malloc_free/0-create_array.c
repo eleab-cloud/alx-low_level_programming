18d1b3c79e41abd4d95d814c947ab2b6f36ce401
eleab cloud <eleniababu@gmail.com>
2023-03-14 04:14:22 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	str = malloc(sizeof(char) * size);
 
 	if (str == NULL || size == 0)
-	{
+
 		return (NULL);
-	}
+
 	for (i = 0; i < size; i++)
-	{
+
 		str[i] = c;
-	}
+
 	return (str);
 }
