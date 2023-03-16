5e73dcab441fb67a8763dc739772da2f2d0073cb
eleab cloud <eleniababu@gmail.com>
2023-03-15 23:34:29 -0700

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- intrance
+ * @argc: one arg
+ * @argv: another arg
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (argc = 1; argc < 3; argc++)
+	{
+		if (!(isdigit(argv[argc])))
+		{
+			printf("Error\n");
+			return (1);
+			break;
+		}
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 + num2;
+
+	printf("%d\n", result);
+	return (0);
+}
