83cec8ad6640f6449a13dc0531de1cd45ddaee51
eleab cloud <eleniababu@gmail.com>
2023-03-16 05:57:18 -0700

    malloc func

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * malloc_checked- that allocates memory
+ * @b: size
+ * Return: a pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *str;
+
+	str = malloc(b);
+
+	if (str == NULL)
+		return (98);
+	return (str);
+}
