5793bc22a5e2af3ba7978a8f3ac50be96918588a
eleab cloud <eleniababu@gmail.com>
2023-03-17 05:23:44 -0700

    concat malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,16 +11,16 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	int l1 = 1;
-	int l2 = 1;
+	int l1 = 0;
+	int l2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*s1 != '\0')
+	while (s1[l1] != '\0')
 		l1++;
-	while (*s2 != '\0')
+	while (s2[l2] != '\0')
 		l2++;
 	if (n < l2)
 		str = malloc(l1 + n + 1);
@@ -31,22 +31,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-
-	int i, j;
-
-	for (i = 0; i < l1; i++)
+	else
 	{
-		str[i] = s1[i];
-	}
+		int i, j;
 
-	while (*s2 != '\0')
-	{
+		for (i = 0; i < l1; i++)
+		{
+			str[i] = s1[i];
+		}
 		for (j = 0; j < n; j++)
 		{
 			str[i] = s2[j];
 			i++;
 		}
-	}
 	str[i] = '\0';
+	}
 	return (str);
 }
