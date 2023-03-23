51aaa9c5b139242a3dcf53a2a4e49c9519b5cdea
eleab cloud <eleniababu@gmail.com>
2023-03-23 04:40:31 -0700

    print  struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
- * print_dog - prints print dog type 
+ * print_dog - prints print dog type
  * @d: a pointer to struct dog type
  * Return: nothing
  */
