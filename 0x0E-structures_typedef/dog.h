feb0a61901eb8ce4f9261acfe72c22a683a14559
eleab cloud <eleniababu@gmail.com>
2023-03-23 03:51:51 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner
+ *
+ * Description: to hold dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
