039cebc03d85bb21d1bd65278333403280f1cb01
eleab cloud <eleniababu@gmail.com>
2023-03-23 04:38:10 -0700

    printing structure values

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints print dog type 
+ * @d: a pointer to struct dog type
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name:%s\nAge: %S\nOwner: %s\n", d->name, d->age, d->owner);
+}
+
