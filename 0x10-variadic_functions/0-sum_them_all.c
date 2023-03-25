0a456be4ed92073d5ae74e9fc625f87fdb464005
eleab cloud <eleniababu@gmail.com>
2023-03-25 07:22:42 -0700

    vardiac sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	if (n == 0)
 		return (0);
-	unsigned int i, sum = 0;
+	unsigned int i;
+
+	unsigned int sum = 0;
 
 	va_list args;
 
@@ -23,5 +25,3 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
-
-
