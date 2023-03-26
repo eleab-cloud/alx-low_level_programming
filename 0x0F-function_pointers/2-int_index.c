730a453fe0a254fd885a55fa137d04150c45f81c
eleab cloud <eleniababu@gmail.com>
2023-03-26 06:24:57 -0700

    compare an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (index = 0; index < size; index++)
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
-		else
-			return (-1);
 	}
+	return (-1);
 }
