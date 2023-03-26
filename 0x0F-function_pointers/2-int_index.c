43dcc25144bc9f53ff5bee997f6332fd47d159ea
eleab cloud <eleniababu@gmail.com>
2023-03-26 06:13:31 -0700

    compare an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index- returns a non integer
+ * @array: to be executed
+ * @size: size of the array
+ * @cmp: the function to be called
+ * Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int index;
+
+	if (size <= 0)
+		return (-1);
+	for (index = 0; index < size; i++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+		else
+			return (-1);
+	}
+}
