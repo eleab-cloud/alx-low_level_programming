033ef98291ed06cff26597df88ed067e3b581de9
eleab cloud <eleniababu@gmail.com>
2023-04-03 08:27:10 -0700

    linked list more

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_nodeint- add a node at head of list
+ * head: pointer to first node
+ * @n: value of the node
+ * Return: the pointer to new hed node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newhead;
+
+	if (head == NULL)
+		return (NULL);
+
+	newhead = malloc(sizeof(listint_t));
+
+	if (newhead == NULL)
+		return (NULL);
+	newhead->n = n;
+	newhead->next = (*head);
+	(*head) = newhead;
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_nodeint_end
+ * @head: pointer to head of a struct node
+ * @n: an element of a node
+ * Return: pointer to the list head
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newtail;
+
+	listint_t *temp;
+
+	temp = *head;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	newtail = malloc(sizeof(listint_t));
+	if (newtail == NULL)
+	{
+		return (NULL);
+	}
+	newtail->n = n;
+	newtail->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newtail;
+		return (newtail);
+	}
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = newtail;
+	return (newtail);
+}
