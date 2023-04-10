1b66fef9fb08fa2f682f720c444bf8efb5b46eab
eleab cloud <eleniababu@gmail.com>
2023-04-10 05:00:52 -0700

    clearing a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * clear_bit- set the bit at index
+ * @n: teh number pointer
+ * @index: teh index
+ * Return: the bit at index
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	if (index < 64)
+	{
+		*n = *n & 0 << index;
+		return (1);
+	}
+	return (-1);
+}
