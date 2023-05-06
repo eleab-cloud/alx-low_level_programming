3e76a70726ee356db358cac2e974cf12c9b28623
eleab cloud <eleniababu@gmail.com>
2023-05-06 04:59:13 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN-H
+#define MAIN_H
 int get_endianness(void);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
