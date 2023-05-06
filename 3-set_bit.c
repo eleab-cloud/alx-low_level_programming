95e2e04ad67b9c2e202465f293d184fa55edb99e
eleab cloud <eleniababu@gmail.com>
2023-05-06 04:47:32 -0700

    bit manipulation

---
+++ 3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * set_bit- set the bit at index
+ * @n: teh number pointer
+ * @index: teh index
+ * Return: the bit at index
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	if (index < 64)
+	{
+		*n = *n | 1 << index;
+		return (1);
+	}
+	return (-1);
+}
