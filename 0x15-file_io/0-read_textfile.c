e9b5a5b133e9ecc949f5665f768ec67db75e2460
eleab cloud <eleniababu@gmail.com>
2023-05-08 02:18:03 -0700

    io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * read_textfile- reads a file
+ * @filename: the file to be read
+ * @letters: the bytes to be read
+ * Return: number of letters it could read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+
+	ssize_t w;
+
+	ssize_t sz;
+
+	char *R;
+
+	R = malloc(sizeof(char) * letters);
+
+	if (R == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	sz = read(fd, R, letters);
+	if (sz == -1)
+	{
+		return (0);
+	}
+	w = write(STDOUT_FILENO, R, sz);
+	free(R);
+	close(fd);
+	return (w);
+}
